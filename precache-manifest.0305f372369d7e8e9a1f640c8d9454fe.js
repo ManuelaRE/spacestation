2db1d8df9119b5a451c9e958f2542879b29d7e57
Manuela Redinciuc <46057566+ManuelaRE@users.noreply.github.com>
2020-06-03 14:52:13 +0100

    Updates

---
+++ precache-manifest.0305f372369d7e8e9a1f640c8d9454fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d70442ab0aee75ba091429b8cbeb86e",
+    "url": "/spacestation/index.html"
+  },
+  {
+    "revision": "7d66ef239fd2e5125370",
+    "url": "/spacestation/static/css/main.a74b8fef.chunk.css"
+  },
+  {
+    "revision": "05e67f532588f0ff66a1",
+    "url": "/spacestation/static/js/2.5347fa48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spacestation/static/js/2.5347fa48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d66ef239fd2e5125370",
+    "url": "/spacestation/static/js/main.9f146b85.chunk.js"
+  },
+  {
+    "revision": "a8bba4537a9ddaff88d6",
+    "url": "/spacestation/static/js/runtime-main.4f841a93.js"
+  },
+  {
+    "revision": "e8b6413bfe2217496beda90ff5cf1f5c",
+    "url": "/spacestation/static/media/Citrica-Regular.e8b6413b.ttf"
+  },
+  {
+    "revision": "b2c65c8a2a051fa6df711674e9c3b582",
+    "url": "/spacestation/static/media/photo.b2c65c8a.jpg"
+  },
+  {
+    "revision": "bfef1d6d956d796fe783ebf50dfb7a89",
+    "url": "/spacestation/static/media/video.bfef1d6d.mp4"
+  }
+]);
